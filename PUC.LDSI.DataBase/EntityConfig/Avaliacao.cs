8bf7aa219a7aecf5e2a6f0094eb5596bdbc4a5d9
CarolinaStarling <starlingcarolina@gmail.com>
2020-03-07 11:36:11 -0300

    Criação das classes de configuração.

---
+++ PUC.LDSI.DataBase/EntityConfig/Avaliacao.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PUC.LDSI.Domain.Entities;
+
+namespace PUC.LDSI.DataBase.EntityConfig
+{
+    public class EntityConfiguration : IEntityTypeConfiguration<Avaliacao>
+    {
+        public void Configure(EntityTypeBuilder<Avaliacao> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseSqlServerIdentityColumn();
+            builder.Property(x => x.DataCriacao).IsRequired().HasColumnType("datetime");
+        }
+    }
+}
---
+++ PUC.LDSI.DataBase/EntityConfig/Questao.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PUC.LDSI.Domain.Entities;
+
+namespace PUC.LDSI.DataBase.EntityConfig
+{
+    public class EntityConfiguration : IEntityTypeConfiguration<Questao>
+    {
+        public void Configure(EntityTypeBuilder<Questao> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseSqlServerIdentityColumn();
+            builder.Property(x => x.DataCriacao).IsRequired().HasColumnType("datetime");
+        }
+    }
+}
