452200c2cac8696cbb5aa5b117c31628e05ef93a
carolinaromanzini <carolina.romanzini@sga.pucminas.br>
2020-03-07 12:30:08 -0300

    Teste1

--- PUC.LDSI.Domain/Entities/Aluno.cs
+++ PUC.LDSI.Domain/Entities/Aluno.cs
@@ -16,6 +16,7 @@ namespace PUC.LDSI.Domain.Entities
         public DateTime DataCriacao { get; set; }
         public varchar Nome { get; set; }
         public int Turma { get; set; }
+
         
 
     }
