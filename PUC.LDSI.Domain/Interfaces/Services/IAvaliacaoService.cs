7dfe3e17fa89c4a9845e71588ee23fca878b8532
Weslei Oliveira dos Santos <1076996@sga.pucminas.br>
2020-04-08 00:07:46 -0300

    Cadastro de Avaliações - Repositórios

---
+++ PUC.LDSI.Domain/Interfaces/Services/IAvaliacaoService.cs
@@ -0,0 +1,16 @@
+﻿using PUC.LDSI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PUC.LDSI.Domain.Interfaces.Services
+{
+    public interface IAvaliacaoService
+    {
+        Task<int> AdicionarAvaliacaoAsync(string descricao);
+        Task<int> AlterarAvaliacaoAsync(int id, string descricao);
+        List<Avaliacao> ListarAvaliacoes();
+        Task<Avaliacao> ObterAsync(int id);
+        Task ExcluirAsync(int id);
+    }
+}
---
+++ PUC.LDSI.Domain/Interfaces/Services/IOpcaoAvaliacao.cs
@@ -0,0 +1,16 @@
+﻿using PUC.LDSI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PUC.LDSI.Domain.Interfaces.Services
+{
+    public interface IOpcaoAvaliacao
+    {
+        Task<int> AdicionarOpcaoAvaliacaoAsync(string descricao);
+        Task<int> AlterarOpcaoAvaliacaoAsync(int id, string descricao);
+        List<Avaliacao> ListarOpcoesavaliacao();
+        Task<Avaliacao> ObterAsync(int id);
+        Task ExcluirAsync(int id);
+    }
+}
