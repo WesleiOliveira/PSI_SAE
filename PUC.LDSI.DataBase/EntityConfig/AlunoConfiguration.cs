690626b6bfa7d57346efca5f4a29cb7bfd846d53
Weslei Oliveira dos Santos <1076996@sga.pucminas.br>
2020-04-17 14:58:49 -0300

    correção Classe Aluno

--- PUC.LDSI.DataBase/EntityConfig/AlunoConfiguration.cs
+++ PUC.LDSI.DataBase/EntityConfig/AlunoConfiguration.cs
@@ -17,7 +17,9 @@ namespace PUC.LDSI.DataBase.EntityConfig
             //HasOne(x => x.Turma) = Indica que Turma é o atributo de relacionamento (Aluno possui uma Turma)
             //WithMany(x => x.Alunos) = Indica que a lista Alunos é o atributo de relacionamento dentro da entidade relacionada (Turma possui varios Alunos)
             //HasForeignKey(x => x.TurmaId) = Indica que o atributo TurmaId deve ser considerado como a chave estrangeira (foreign key)
-            builder.HasOne(x => x.Turma).WithMany(x => x.Alunos).HasForeignKey(x => x.TurmaId);
+            builder.HasOne(x => x.Turma)
+                   .WithMany(x => x.Alunos)
+                   .HasForeignKey(x => x.TurmaId);
 
             //Importante: Aplica as configurações dos atributos comuns que estão na entidade pai Entity
             new EntityConfiguration();
