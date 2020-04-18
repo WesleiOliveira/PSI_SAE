938e399414f00cedc7575449a36e05dd0f49df6c
Paloma Castro <pmcastro@sga.pucminas.br>
2020-04-18 10:23:53 -0300

    Identity

--- PUC.LDSI.IoC/NativeBootStrapperBase.cs
+++ PUC.LDSI.IoC/NativeBootStrapperBase.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using PUC.LDSI.Application.AppServices;
+using PUC.LDSI.Application.Interfaces;
+using PUC.LDSI.DataBase.Repository;
+using PUC.LDSI.Domain.Interfaces.Repository;
+using PUC.LDSI.Domain.Interfaces.Services;
+using PUC.LDSI.Domain.Services;
 
 namespace PUC.LDSI.IoC
 {
     public abstract class NativeBootStrapperBase
     {
         public static void RegisterServices(IServiceCollection services) {
-            //Example
-            //services.AddScoped<IClasseAppService, ClasseAppService>();
+            
+            services.AddScoped<IAlunoRepository, AlunoRepository>();
+
+            services.AddScoped<IProfessorAppService, ProfessorAppService>();
+
+            services.AddScoped<ITurmaAppService, TurmaAppService>();
+            
+            //Application
+            services.AddScoped<ITurmaAppService, TurmaAppService>();
+
+            //Domain - Repository
+            services.AddScoped<ITurmaRepository, TurmaRepository>();
+
+            //Domain - Services
+            services.AddScoped<ITurmaService, TurmaService>();
         }
     }
 }
