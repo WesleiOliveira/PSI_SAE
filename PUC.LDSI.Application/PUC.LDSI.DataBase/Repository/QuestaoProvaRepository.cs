76b204f38e6966533b37d0b610028872a37ebd21
Carol <carolina.romanzini@sga.pucminas.br>
2020-06-05 14:21:25 -0300

    v1 repository

---
+++ PUC.LDSI.Application/PUC.LDSI.DataBase/Repository/QuestaoProvaRepository.cs
@@ -0,0 +1,10 @@
+﻿using PUC.LDSI.Domain.Entities;
+using PUC.LDSI.Domain.Interfaces.Repository;
+
+namespace PUC.LDSI.DataBase.Repository
+{
+    public class QuestaoProvaRepository : Repository<QuestaoProva>, IQuestaoProvaRepository
+    {
+        public QuestaoProvaRepository(AppDbContext context) : base(context) { }
+    }
+}
