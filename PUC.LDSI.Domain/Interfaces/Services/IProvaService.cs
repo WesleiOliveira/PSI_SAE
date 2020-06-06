910e5c45698538f9b0e1cd01a595f3f14e9cf8f3
Weslei Oliveira dos Santos <1076996@sga.pucminas.br>
2020-06-05 21:52:41 -0300

    cvv

---
+++ PUC.LDSI.Domain/Interfaces/Services/IProvaService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PUC.LDSI.Domain.Interfaces.Services
+{
+    public interface IProvaService
+    {
+        Task<int> IniciarRealizacaoDaProvaAsync(int avaliacaoId, int alunoId);
+        Task GravarQuestaoProvaAsync(int questaoId, List<Entities.OpcaoProva> respostas);
+        Task<int> ConcluirProvaAsync(int provaId);
+        Task<List<Entities.Publicacao>> ListarProvasPublicadasDoAlunoAsync(int alunoId);
+    }
+}
---
+++ PUC.LDSI.Domain/Services/ProvaService.cs
@@ -0,0 +1,33 @@
+﻿using PUC.LDSI.Domain.Entities;
+using PUC.LDSI.Domain.Exception;
+using PUC.LDSI.Domain.Interfaces.Repository;
+using PUC.LDSI.Domain.Interfaces.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PUC.LDSI.Domain.Services
+{
+    public class ProvaService : IProvaService
+    {
+        public async Task<int> IniciarRealizacaoDaProvaAsync(int avaliacaoId, int alunoId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<int> ConcluirProvaAsync(int provaId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task GravarQuestaoProvaAsync(int questaoId, List<OpcaoProva> respostas)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        
+        public async Task<List<Publicacao>> ListarProvasPublicadasDoAlunoAsync(int alunoId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
